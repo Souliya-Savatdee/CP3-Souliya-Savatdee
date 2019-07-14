f42c4f9c427a91c2b3ef1ecece696ae71f0f9d7a
Souliya-Savatdee <52776795+Souliya-Savatdee@users.noreply.github.com>
2019-07-14 16:29:20 +0700

    Add files via upload

---
+++ Exercise5_1_Souliya_S.py
@@ -0,0 +1,10 @@
+'''
+Exercise5_Ep1
+'''
+x = Num1 = int(input("plase enter your first number :"))
+y = Num2 = int(input("plase enter your second number2 :"))
+
+print(x,"+",y,"=",(x+y))
+print(x,"-",y,"=",(x-y))
+print(x,"*",y,"=",(x*y))
+print(x,"/",y,"=",(x/y))
---
+++ Exercise5_2_Souliya_S.py
@@ -0,0 +1,8 @@
+'''
+Exercise5_Ep2
+'''
+s = Distance = int(input("plase enter your distance (Km):"))
+t = Time = int(input("plase enter your Time (h):"))
+v = velovcity = int(s/t)
+
+print("velovcity :",v,"(Km/h)")
