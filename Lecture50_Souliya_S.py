7b2ba99f0d7c0bafc39ff311a0f680f5d6e9dacf
Souliya-Savatdee <52776795+Souliya-Savatdee@users.noreply.github.com>
2019-07-19 16:09:55 +0700

    Add files via upload

---
+++ Lecture50_Souliya_S.py
@@ -0,0 +1,15 @@
+Number1 = int(input("Plase rnter your first Number :"))
+Number2 = int(input("Plase rnter your second Number :"))
+def A_Number(Number1,Number2):
+    print("Result :",Number1,"+",Number2,"=",Number1+Number2)
+def B_Number(Number1,Number2):
+    print("Result :",Number1,"-",Number2,"=",Number1-Number2)
+def C_Number(Number1,Number2):
+    print("Result :",Number1,"*",Number2,"=",Number1*Number2)
+def D_Number(Number1,Number2):
+    print("Result :",Number1,"/",Number2,"=",Number1/Number2)
+
+A_Number(Number1,Number2)
+B_Number(Number1,Number2)
+C_Number(Number1,Number2)
+D_Number(Number1,Number2)
